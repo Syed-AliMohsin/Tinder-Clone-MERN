fa456290f82cf3ee5865b186494bc2f4d111d398
Ali Mohsin <syedalimohsin987@gmail.com>
2024-12-13 13:11:33 +0500

    1. some changes in server.js file

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { createServer } from "http";
 
 // routes
@@ -11,12 +12,17 @@ import matchRoutes from "./routes/matchRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 
 import { connectDB } from "./config/db.js";
+import { initializeSocket } from "./socket/socket.server.js";
 
 dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+initializeSocket(httpServer);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,8 +38,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/matches", matchRoutes);
 app.use("/api/messages", messageRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
 httpServer.listen(PORT, () => {
   console.log("Server started at this port:" + PORT);
   connectDB();
 });
-
